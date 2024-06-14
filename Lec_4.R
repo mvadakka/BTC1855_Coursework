f10bee67c51aedbf38655a5f3bc2376827bdfad7
Mausam <mvadakka@uwo.ca>
2024-06-14 17:46:42 -0400

    sigma calculations

--- Lec_4.R
+++ Lec_4.R
@@ -15,4 +15,26 @@ if(x == 0){
   print(paste(x, "is not 0. Try again."))
 } #if x ==0, print x is 0, otherwise say x is not 0 and try again
 
+##### calculating sigma by:
+#' if True
+#'  calculate sigma
+#'if F
+#'  exit gracefully 
+#calculate sigma
+# output sigma
+
+#1. read user input 
+user.input <- readline(prompt = "Please enter a number less than 1000")
+#2. convert user input to numeric
+numeric.input <- as.numeric(user.input)
+
+#3. check if numeric is na (non-number value)
+if(is.na(numeric.input)) {
+  print(paste(user.input,"is not a number. Please try again:")) #4. check if numeric is less than 1000, if not try again
+} else if (numeric.input > 1000) {
+  print(paste(numeric.input,"is too large. Try a number less than 1000:")) # find sigma of numeric and print
+} else {
+  print(sum(1:numeric.input))
+}
+
 
