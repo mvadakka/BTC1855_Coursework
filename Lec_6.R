1811c72d336a1144188220b15644204ef3063a7e
Mausam <mvadakka@uwo.ca>
2024-06-27 17:41:35 -0400

    date, cat function

---
+++ Lec_6.R
@@ -0,0 +1,20 @@
+# Sample functions for data wrangling
+
+today1 <- Sys.Date()
+today2 <- date()
+
+now <- Sys.time()
+
+#convert from POSIX to string 
+new_now <- strftime(now, "%a, %B, %d - %H:%M:%S %Y") 
+
+#convert from string to POSIX
+new_today <- strptime(today2, "%a, %B, %d - %H:%M:%S %y")
+
+later <- Sys.time()
+difftime(later, now, units = "hours")
+
+#multiline message to user 
+sentence <- "this is the first line. \nThis is the second line"
+print(sentence)
+cat(sentence)
